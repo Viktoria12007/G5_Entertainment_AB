116e7b8c38ef0da406dcd387adfbc0ff70928d43
viki1 <v.golubova@denexy.com>
2023-01-29 17:13:05 +0400

    Add import new components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template lang="pug">
   div(id="app")
     MainHeader
+    main.main__wrap
+      BannerSlider
+      ProductsList
 </template>
 
 <script>
 import MainHeader from './components/MainHeader.vue'
+import BannerSlider from '@/components/BannerSlider'
+import ProductsList from '@/components/ProductsList'
 
 export default {
   name: 'App',
   components: {
+    ProductsList,
+    BannerSlider,
     MainHeader
   }
 }
 </script>
 
 <style lang="sass">
-
+.main__wrap
+  padding: 8px 0px
 </style>
