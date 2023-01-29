bfddcfe3ce8992ca4b9955da63cef24b89c1dca5
viki1 <v.golubova@denexy.com>
2023-01-29 17:11:10 +0400

    Restyle MainHeader.vue

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
-  header.header
+header.header
+  div.container
+    a.logo__link.hide(href="#")
+      img.logo__img(src="../assets/logo.png", alt="Логотип")
+    button Update Products
 </template>
 
 <script>
@@ -10,5 +14,8 @@ export default {
 
 <style lang="sass">
 .header
- background-color: red
+  display: flex
+  justify-content: space-between
+  height: 48px
+  background-color: $primary
 </style>
